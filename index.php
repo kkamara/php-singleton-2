945d7031c3b605f0665d14bf5178b406826cfac1
kkamara <kamaracomputers@gmail.com>
2023-12-12 14:35:33 +0000

    Add trait

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+trait Singleton {
+    public static function get_instance() {
+        static $instance = [];
+
+        $called_class = get_called_class();
+
+        if (!isset($instance[$called_class])) {
+            $instance[$called_class] = new $called_class;
+        }
+
+        return $instance[$called_class];
+    }
+}
