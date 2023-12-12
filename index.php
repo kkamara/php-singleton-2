f5d6e67bb89d54e1e56967501a4056b4e1bfb417
kkamara <kamaracomputers@gmail.com>
2023-12-12 14:39:25 +0000

    Add User class

--- index.php
+++ index.php
@@ -7,9 +7,21 @@ trait Singleton {
         $called_class = get_called_class();
 
         if (!isset($instance[$called_class])) {
+            echo "Check happens here".PHP_EOL;
             $instance[$called_class] = new $called_class;
         }
 
         return $instance[$called_class];
     }
 }
+
+class User {
+    use Singleton;
+
+    public function __construct() {
+        echo "User".PHP_EOL;
+    }
+}
+
+$user_one = User::get_instance();
+$user_two = User::get_instance();
